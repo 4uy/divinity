6b52f5190cb6415e44a11d621bd02c8bb69b3cf1
4uy <64391250+4uy@users.noreply.github.com>
2021-12-30 16:02:05 -0600

    Rename botinfo command to info

--- src/commands/info.js
+++ src/commands/info.js
@@ -3,13 +3,13 @@ const { MessageEmbed } = require("discord.js");
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName("botinfo")
+		.setName("info")
 		.setDescription("Get information about the bot."),
 	async execute(interaction, client) {
         const userCount = await client.guilds.cache.map((guild) => guild.memberCount).reduce((p, c) => p + c)
 
         const embed = new MessageEmbed()
-        .setTitle("Bot Info")
+        .setTitle("Bot Information")
         .setThumbnail(client.user.displayAvatarURL())
         .addField("Server Count", `${client.guilds.cache.size}`, true)
         .addField("Total Users", `${userCount}`, true)
