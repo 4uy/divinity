5fb0cab15b4732cbe2c8c0e38a2c04a797ea5339
4uy <64391250+4uy@users.noreply.github.com>
2021-12-29 18:36:33 -0600

    Added base discord.js stuff

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("Hello, world!");
+const { Client, Intents } = require("discord.js");
+require("dotenv").config();
+
+const client = new Client({
+	intents: [Intents.FLAGS.GUILDS]
+});
+
+client.once("ready", () => {
+    console.log(`Logged in as ${client.user?.tag} (${client.user?.id})`);
+});
+
+client.login(process.env.TOKEN);
